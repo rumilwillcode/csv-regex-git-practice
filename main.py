6379fd4a6a02e2720f08cf34013771d56023245a
Rumil G. <rumilbukhara@yahoo.com>
2024-12-16 16:48:54 +0300

    Added unhappy_customer function (work in progress).

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-"""Main.py"""
+import csv
+import re
+
+
+def unhapy_customer():
+  """Takes in feedback.txt to identify unhappy customers (that rated us lower than 4 out of 5) and reaturns
+     the customer ID, text of feedback and customer's phone #."""
+  with open("feedback.txt", "r") as file:
+
+    #Initialize customer_id, feedback, and phone_num lists to be populated with data.
+    customer_id = []
+    feedback = []
+    phone_num = []
+    
+    reader = csv.reader(file) 
