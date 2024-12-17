8ec86f6d8d2d90703b63275790239d96b3d0c117
Rumil G. <rumilbukhara@yahoo.com>
2024-12-17 16:20:51 +0300

    Completed unhappy customer function, formatted feedback.txt.

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ def unhappy_customer():
   """Takes in feedback.txt to identify unhappy customers (that rated us lower than 4 out of 5) and returns
      the customer ID, text of feedback and customer's phone #."""
   with open("feedback.txt", "r") as file:
-
-    #Initialize customer_id, feedback, and phone_num lists to be populated with data.
     customer_id = []
     feedback = []
     phone_num = []
+    rating = []
     
-    reader = csv.reader(file)
+    for line in file:
+      pattern = r"^([\d]+) +([A-Za-z\., ]+) +(\d) +([\d\-() ]*)"
+      match = re.search(pattern, line)
+      if match and int(match.group(3)) < 4:
+        customer_id.append(match.group(1).strip())
+        feedback.append(match.group(2).strip())
+        rating.append(match.group(3).strip())
+        phone_num.append(match.group(4).strip())
+  return customer_id, feedback, rating, phone_num
+
+unhappy_customer()
