add343786c2f904cbae7e56cc53622ee9f980031
Rumil G. <rumilbukhara@yahoo.com>
2024-12-17 16:55:39 +0300

    Add main() function that executes the main.py script.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import re
 def unhappy_customer():
   """Takes in feedback.txt to identify unhappy customers (that rated us lower than 4 out of 5) and returns
      the customer ID, text of feedback and customer's phone #."""
+
   with open("feedback.txt", "r") as file:
     customer_id = []
     feedback = []
@@ -23,9 +24,20 @@ def unhappy_customer():
 
 def create_call_list(customer_id, feedback, rating, phone_num):
   """Using the data extracted by the unhappy_customer function, creates a call list of unhappy customers (csv file)."""
+
   with open("unhappy_call_list.csv", "w", newline="") as file:
     writer = csv.writer(file)
     columns = ["Customer ID", "Feedback", "Rating", "Phone #"] 
     writer.writerow(columns)
     for i in range(len(customer_id)):
-      writer.writerow([customer_id[i], feedbsck[i], rating[i], phone_num[i]])
+      writer.writerow([customer_id[i], feedback[i], rating[i], phone_num[i]])
+
+def main():
+  """Executes unhappy_customer and create_call_list functions."""
+
+  customer_id, feedback, rating, phone_num = unhappy_customer()
+  create_call_list(customer_id, feedback, rating, phone_num)
+
+if __name__ == "__main__":
+  main()
+ 
